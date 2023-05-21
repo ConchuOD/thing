a0fb3d55aab50562055f0c10820bce9dcf2f8d95
Conor Dooley <mail@conchuod.ie>
2023-05-21 22:43:40 +0100

    main: allow passing the entry point & load address
    Signed-off-by: Conor Dooley <mail@conchuod.ie>

--- main.rs
+++ main.rs
@@ -20,18 +20,34 @@ struct Args
 {
 	/// input binary
 	#[clap(short, long, default_value = "hi.elf")]
-	elf: String,
+	blob: String,
+
+	/// load address
+	#[clap(short, long)]
+	load_address: Option<usize>,
+
+	/// entry point
+	#[clap(short, long)]
+	entry_point: Option<usize>,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>>
 {
 	let args = Args::parse();
-	let elf: Vec<u8> = fs::read(args.elf)?;
+	let blob: Vec<u8> = fs::read(args.blob)?;
+	let mut load_address: usize = 0x0;
+	let mut entry_point: usize = 0x164;
+
+	if args.load_address.is_some() {
+		load_address = args.load_address.unwrap();
+	}
+
+	if args.entry_point.is_some() {
+		entry_point = args.entry_point.unwrap();
+	}
 
 	let mut platform: Platform = Platform::default();
 
-	let entry_point: usize = 0x164;
-
-	platform.load_file(elf, 0, entry_point)?;
+	platform.load_file(blob, load_address, entry_point)?;
 	return platform.emulate();
 }
